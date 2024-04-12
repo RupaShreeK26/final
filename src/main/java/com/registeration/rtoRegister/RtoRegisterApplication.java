0aa0b569eaf469fc0bec42a717ebc4708e4ab858
RupaShreeK26 <krupashree2604@gmail.com>
2024-04-12 20:14:01 +0530

    Rto

---
+++ src/main/java/com/registeration/rtoRegister/RtoRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.registeration.rtoRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtoRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtoRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/registeration/rtoRegister/RtoRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registeration.rtoRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtoRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
